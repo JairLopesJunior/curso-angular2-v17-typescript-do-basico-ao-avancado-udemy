931479ef34d69e84ce1d64f55448b4514fc18f39
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2025-02-20 10:38:27 -0300

    feat: Control Flow - @if, @else if e @else

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ControlFlowIfComponent } from '../control-flow/control-flow-if/control-flow-if.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ControlFlowIfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ meu-primeiro-projeto-latest/src/control-flow/control-flow-if/control-flow-if.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flow-if',
+  standalone: true,
+  imports: [],
+  templateUrl: './control-flow-if.component.html',
+  styleUrl: './control-flow-if.component.scss'
+})
+export class ControlFlowIfComponent {
+
+  isTrue: boolean = false;
+}
