19ffc1277b94af74468e4645121b562333b4c8e9
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2025-02-24 14:07:53 -0300

    feat: @Switch CASE

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ControlFlowIfComponent } from '../control-flow/control-flow-if/control-flow-if.component';
 import { ControlFlowForComponent } from '../control-flow/control-flow-for/control-flow-for.component';
+import { ControlFlowSwitchComponent } from '../control-flow/control-flow-switch/control-flow-switch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ControlFlowIfComponent, ControlFlowForComponent],
+  imports: [
+    RouterOutlet,
+    ControlFlowIfComponent,
+    ControlFlowForComponent,
+    ControlFlowSwitchComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ meu-primeiro-projeto-latest/src/control-flow/control-flow-switch/control-flow-switch.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flow-switch',
+  standalone: true,
+  imports: [],
+  templateUrl: './control-flow-switch.component.html',
+  styleUrl: './control-flow-switch.component.scss'
+})
+export class ControlFlowSwitchComponent {
+
+  switchCondition = "Bv";
+}
